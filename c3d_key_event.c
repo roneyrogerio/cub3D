6ac9e1f4cfca114a8fa6f0c44f7532cd8cfac456
Roney <roneyrogerio92@gmail.com>
2020-07-20 13:30:36 -0300

    submodule udated

--- c3d_key_event.c
+++ c3d_key_event.c
@@ -6,7 +6,7 @@
 /*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/29 22:10:15 by rde-oliv          #+#    #+#             */
-/*   Updated: 2020/06/24 07:05:45 by rde-oliv         ###   ########.fr       */
+/*   Updated: 2020/07/20 11:52:31 by rde-oliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ int		c3d_key_press(int code, t_c3d *c3d)
 {
 	if (code == 65307)
 		c3d_quit(EXIT_SUCCESS);
-	if (code == 115)
+	else if (code == 115)
 		ngn_set_walk(c3d->ngn, 1);
-	if (code == 65361)
+	else if (code == 65361)
 		ngn_set_turn(c3d->ngn, -1);
-	if (code == 119)
+	else if (code == 119)
 		ngn_set_walk(c3d->ngn, -1);
-	if (code == 65363)
+	else if (code == 65363)
 		ngn_set_turn(c3d->ngn, 1);
-	if (code == 97)
-		ngn_set_side(c3d->ngn, -1);
-	if (code == 100)
-		ngn_set_side(c3d->ngn, 1);
+	else if (code == 97)
+		ngn_set_turn(c3d->ngn, -1);
+	else if (code == 100)
+		ngn_set_turn(c3d->ngn, 1);
 	return (0);
 }
 
@@ -35,15 +35,15 @@ int		c3d_key_release(int code, t_c3d *c3d)
 {
 	if (code == 119)
 		ngn_set_walk(c3d->ngn, 0);
-	if (code == 65361)
+	else if (code == 65361)
 		ngn_set_turn(c3d->ngn, 0);
-	if (code == 115)
+	else if (code == 115)
 		ngn_set_walk(c3d->ngn, 0);
-	if (code == 65363)
+	else if (code == 65363)
 		ngn_set_turn(c3d->ngn, 0);
-	if (code == 97)
-		ngn_set_side(c3d->ngn, 0);
-	if (code == 100)
-		ngn_set_side(c3d->ngn, 0);
+	else if (code == 97)
+		ngn_set_turn(c3d->ngn, 0);
+	else if (code == 100)
+		ngn_set_turn(c3d->ngn, 0);
 	return (0);
 }
